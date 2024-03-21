119a1b212f8fe84e0dda82987a71bfc54bce0dd8
lucasjrclarke <lukeyspud@gmail.com>
2024-03-21 11:17:05 +1100

    Added higher res images, contact details+board, gallery and first prod ready

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,10 +2,10 @@
 
 const Board = () => {
 
-  const boardMemberNames = ["Tyler Daniels", "Tyler Damiels"];
-  const boardMemberRoles = ["President", "President 2"];
-  const boardMemberEmails = ["president@wwfc.com.au", "president2@wwfc.com.au"];
-  const boardMemberImages = ["default-profile-pic.png", "default-profile-pic.png"];
+  const boardMemberNames = ["Tyler Daniels", "Amy O'Connor", "Richard Goldsmith", "Jimmy McBride", "Frank Gibellini", "Daniel George", "Annette Felicissimo", "Chris Wellock", "Rob Wardle", "Erica Wardle"];
+  const boardMemberRoles = ["President", "Treasurer", "Chairman", "Head Coach", "Head of Football (U15s+)", "Vice President", "Secretary", "Head of Football (Juniors)", "Kit Coordinator", "Registrar"];
+  const boardMemberEmails = ["president@wwfc.club", "info@wwfc.club", "", "", "", "vicepresident@wwfc.club", "", "", "", "registrar@wwfc.club"];
+  const boardMemberImages = ["commitee/tyler-d.png", "commitee/amy-oc.png", "commitee/richard-g.png", "commitee/jimmy-m.png", "commitee/frank-g.png", "commitee/daniel-g.png", "commitee/annette-f.png", "commitee/chris-w.png", "commitee/rob-w.png", "commitee/erica-w.png"];
 
   
   return (
@@ -15,7 +15,7 @@ const Board = () => {
           <img src={boardMemberImages[index]} alt={`Photo of ${member}`} />
           <p className="bold">{member}</p>
           <p>{boardMemberRoles[index]}</p>
-          <a href={boardMemberEmails[index]}>{boardMemberEmails[index]}</a>
+          <a>{boardMemberEmails[index]}</a>
         </div>
       ))}
     </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,9 +9,9 @@ const Footer = () => {
         </div>
         <div id="footer-details">
             <img src="blue-wizards-footer-logo-1.png" alt="Wonga Park Wizards logo" />
-            <p>President Tyler Daniels</p>
-            <a>president@wongawizards.com.au<br/><br/></a>
-            <a>newemail@wongawizards.com.au<br/><br/></a>
+            <p>President - Tyler Daniels</p>
+            <a>president@wwfc.club</a>
+            <a>0412956961<br/><br/></a>
             <p>Wonga Wizards FC<br/>PO Box 5191, Launders Ave, Wonga Park,<br/>Victoria, Australia, 3115<br/><br/></p>
             <p>Wonga Park Wizards FC. Copyright &#169; 2024. Website by Lucas Clarke.</p>
             <a href="https://docs.google.com/document/d/e/2PACX-1vSA67SkgirGQkURbWmsGkqRxpltuEbHUKAG84Dgw2tghZbjLnisHNe0VxpLOxoVHOJMQY8TqXamGHgS/pub" target="_blank">Privacy Policy</a>
